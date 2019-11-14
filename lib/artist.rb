24c961da0ecbf6d5e8802bf6da336574763b3f72
Patrick Grady <iminvisibl2u@gmail.com>
2019-11-14 22:56:18 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+require "pry"
+
+class Artist
+        # hello = Song.new("Hello")
+        # hello.artist = adele
+        # expect(adele.songs).to eq([hello])
+        
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def songs=(title)
+    new_song = Song.new(title)
+    new_song.artist = @name
+    @songs << new_song
+    
+  end
+  
+  
+  def songs 
+    @songs
+  end
+  
+  
+  
+end
