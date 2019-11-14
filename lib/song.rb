2772b8d682aaa2c3d14634cbc4c6613797c494b2
Patrick Grady <iminvisibl2u@gmail.com>
2019-11-14 23:17:33 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 class Song 
   attr_accessor :title, :artist
   
+  @@all = []
+  
   def initialize(title)
     @title = title
+  end
+  
+  def artist=(artist)
+    @artist = artist
   end
   
 end
