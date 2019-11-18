33376a1f8c2dd060394079de597b850eafa8a9c1
Michael Calkins <m.calkins576@gmail.com>
2019-11-18 18:11:31 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,32 @@
+require './lib/author.rb'
+
+class Post 
+  attr_accessor :title, :author 
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def author=(author_arg)
+    @author = author_arg
+    @author.posts << self
+  end
+  
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
+  
+  
+   
+end
---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+require './lib/artist.rb'
+
+class Song 
+  attr_accessor :name, :artist 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def artist=(artist_arg)
+    @artist = artist_arg
+    @artist.songs << self
+  end
+  
+  def artist_name
+    if @artist == nil
+      return nil
+    else
+      @artist.name
+    end
+  end
+  
+  
+   
+end
