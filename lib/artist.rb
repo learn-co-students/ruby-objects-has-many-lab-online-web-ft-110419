eacbc4a3de6c32a32f9efa5cd59575b876a711a8
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-22 01:20:56 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,24 @@
 require "pry"
 class Artist
-     attr_accessor:name
-  
+     attr_accessor:name,:artist
+     
     def initialize(name)
         @name = name 
         @songs = []
         
     end
-    def songs
-      
-       @songs 
-       #binding.pry
-    end
     
+     def songs
+        Song.all.select do |song| 
+          song.artist == self
+          #binding.pry
+        end
+      end
+     def add_song(song)
+       song.artist = self
+     end
+       def add_song_by_name(name)
+           
+        
+      end
 end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song 
+      attr_accessor:artist,:name
+      @@all = []
+      def initialize(name)
+        @name = name
+         @@all << self
+      end
+      def self.all
+        @@all
+      end
+end
